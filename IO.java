ab82f86cbe5e2765101deb953823df1114e829b4
KamelZyadeh <63470210+KamelZyadeh@users.noreply.github.com>
2020-08-26 11:08:11 +0300

    Delete IO.java

--- IO.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public interface IO {
-
-        public void write(String fileName, String input) throws IOException;
-        public String read(String fileName) throws IOException;
-
-}
