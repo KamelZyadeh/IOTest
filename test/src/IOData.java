133c0db59e654c427270b4064c70a18c9559634e
KamelZyadeh <63470210+KamelZyadeh@users.noreply.github.com>
2020-08-26 11:14:57 +0300

    Update IOData.java

--- test/src/IOData.java
+++ test/src/IOData.java
@@ -3,7 +3,7 @@ public class IOData {
 
     public static IO choose(String string) {
         switch (string) {
-                case "kamel":
+            case "kamel":
                 return new IOStream();
             case "kamel1":
                 return new BufferedIOStream();
